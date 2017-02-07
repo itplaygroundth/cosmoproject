64fc0d3286e803f2f82376d5bb12a44d7da52eef
itplaygroundth <itplayground@hotmail.com>
2017-02-07 20:48:58 +0700

    connect mysql

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
+
 var app=require('express')();
+var path=require('path');
+var mysql=require('mysql');
+
 var port=process.env.PORT || 8082;
+
+var connection=mysql.createConnection({
+host:'192.168.1.117',
+user :'root',
+password:'xxxxxxxx'
+});
+
+connection.query('USE cosmo2017');
+app.set('views',path,join(_dirname,'views'));
+app.set('views engine','jade');
+app.use(express.static(path,join(_dirname,'public')));
+
 app.get('/',function(req,res){
-	res.send('hello world');
+	connection.query('SELECT * FROM users',function(err,rows){
+		res.render('users',{users:rows});
+	})
+	//res.send('hello world');
 });
 app.listen(port,function () {
 	console.log('Start host on'+port);
