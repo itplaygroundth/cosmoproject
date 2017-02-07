defe2e571afa0c5f375446bb52e0a0421a542efa
itplaygroundth <itplayground@hotmail.com>
2017-02-07 22:08:48 +0700

    error views

--- index.js
+++ index.js
@@ -9,17 +9,17 @@ var port=process.env.PORT || 8082;
 var connection=mysql.createConnection({
 host:'localhost',
 user :'cosmo_admin',
-password:'IBMP@w0rd1234'
+password:'IBMP@w0rd1234',
+database:'cosmo2017'
 });
 
-connection.query('USE cosmo2017');
 app.set('views',path.join(__dirname,'views'));
 app.set('view engine','jade');
 app.use(express.static(path.join(__dirname,'public')));
 
 app.get('/',function(req,res){
 	connection.query('SELECT * FROM users',function(err,rows){
-		res.render('users',{users:rows});
+		res.render('users/',{users:rows});
 	})
 	//res.send('hello world');
 });
